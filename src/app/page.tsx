85b7fc6c4250cf9ed21da17934896021da0111ce
morgan bergen <morganmahabergen@icloud.com>
2025-05-19 23:20:06 -0500

    reddi website date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1><code>isitai.net</code></h1>
       <br></br><Image src="/logo.svg" alt="logo" width={300} height={300} priority />
-      <br></br><h1><code>release date july 22 2025</code></h1><br></br>
+      <br></br><h1><code>release date june 22 2025</code></h1><br></br>
     </div>
   );
 }
