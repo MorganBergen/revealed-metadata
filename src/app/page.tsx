56e47730156729ba323c9a78c9f252fd24b6682d
morgan bergen <morganmahabergen@icloud.com>
2025-05-21 15:52:27 -0500

    removed escape sequence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,10 @@
  * @returns JSX.Element
  */
 
-
 export default function Page() {
   return (
     <div>
-      <p><code>
-        
-        Welcome to is it ai, your tool for uncovering the story behind digital images.  Simply upload any supported image file (PNG, JPEG, HEIC) to instantly reveal its embedded metadata.  Explore details about the image's origin, creation, and any modifications, helping you understand its authenticity and provenance. By examining this data, from EXIF and C2PA, you can gain insights into whether an image has been manipulated (edited), has been generated artificially, or was captured by a camera lens. 
-        
-      </code></p>
-      
+      <p><code>Welcome to is it ai, your tool for uncovering the story behind digital images.  Simply upload any supported image file (PNG, JPEG, HEIC) to instantly reveal its embedded metadata.  Explore details about the image&apos;s origin, creation, and any modifications, helping you understand its authenticity and provenance. By examining this data, from EXIF and C2PA, you can gain insights into whether an image has been manipulated (edited), has been generated artificially, or was captured by a camera lens.</code></p>
     </div>
   )
 }
