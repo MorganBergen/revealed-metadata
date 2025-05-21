8d7a6ed6f8497e0b7ec4131cd097a9b692142a6c
morgan bergen <morganmahabergen@icloud.com>
2025-05-21 14:37:58 -0500

    revert npx initialization removing tailwindcss

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
-        <Analytics />
       </body>
     </html>
   );
