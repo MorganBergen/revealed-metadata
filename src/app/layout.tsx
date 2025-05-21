3c516701644bdfe5b86efcdd3fa0c1be9ef40bbb
morgan bergen <morganmahabergen@icloud.com>
2025-05-21 15:45:11 -0500

    homepage iteration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
-import { Analytics } from "@vercel/analytics/next";
+/**
+ * @file    layout.tsx
+ * @brief   Root layout for the application.
+ * @author  morgan bergen
+ * @returns JSX.Element
+ */
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+'use client';  //  render this layout on the client side
+import { useEffect} from 'react';  //  useEffect is a react hook that allows you to perform side effects in function components
+import { useState } from 'react';  //  useState is a react hook that allows you to add state to function components
+import { Analytics } from "@vercel/analytics/next";  //  analytics import from vercel view on dashboard at vercel.com
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: '#FFFFFF' }}>
       <body>
         {children}
         <Analytics />
@@ -14,3 +22,45 @@ export default function RootLayout({
     </html>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FFFFFF
+// F5F5F5
+// EBEBEB
+// E0E0E0
+// D6D6D6
+// CCCCCC
+// C2C2C2
+// B8B8B8
+// ADADAD
+// A3A3A3
+// 999999
+// 8F8F8F
+// 858585
+// 7A7A7A
+// 707070
+// 666666
+// 5C5C5C
+// 525252
+// 474747
+// 3D3D3D
+// 333333
+// 292929
+// 1F1F1F
+// 141414
+// 0A0A0A
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
+
+/**
+ * 
+ * 
+ * 
+ * @returns JSX.Element
+ */
+
+
 export default function Page() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
-      <h1 style={{ fontSize: '2rem', color: '#333', fontFamily: 'monospace' }}>isitai.net</h1>
-      <img src="/0A0A0A-logo.svg" alt="logo" height='300px' />
-      <p style={{ fontSize: '1.5rem', color: '#555', fontFamily: 'monospace' }}>release date july 22 2025</p>
+    <div>
+      <p><code>
+        
+        Welcome to is it ai, your tool for uncovering the story behind digital images.  Simply upload any supported image file (PNG, JPEG, HEIC) to instantly reveal its embedded metadata.  Explore details about the image's origin, creation, and any modifications, helping you understand its authenticity and provenance. By examining this data, from EXIF and C2PA, you can gain insights into whether an image has been manipulated (edited), has been generated artificially, or was captured by a camera lens. 
+        
+      </code></p>
+      
     </div>
   )
 }
