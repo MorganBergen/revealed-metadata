d8cd5db25b49941fd5fe8e546ae24f6147835dcc
morgan bergen <morganmahabergen@icloud.com>
2025-05-20 21:03:50 -0500

    fixed image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1><code>isitai.net</code></h1>
-      <br></br><Image src="/logo.svg" alt="logo" width={300} height={300} priority />
+      <br></br>
+      <Image src="/logo.svg" alt="logo" width={300} height={300} priority />
       <br></br><h1><code>release date june 22 2025</code></h1><br></br>
       <h1><code>update</code></h1>
     </div>
