5dcd0ac8367ccf03e099056bed6edf129c02902f
morgan bergen <morganmahabergen@icloud.com>
2025-06-03 23:08:42 -0700

    welcome text changed as well as hyper link for copy footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
           <div className="homepage-left-column">
             <h1><code>isitai.net</code></h1>
             <p className="welcome-text">
-              In a world of digital edits and AI-generated content, important questions arise about an image&apos;s <Link href="https://en.wikipedia.org/wiki/Provenance">provenance</Link>. (where did this picture come from?  who or what created it?) and authenticity (is this genuine or has it been tampered with?).
+              In a world of digital edits and AI-generated content, important questions arise about an image&apos;s <Link href="https://en.wikipedia.org/wiki/Provenance">provenance</Link> and authenticity.</p> <p className="welcome-text-highlighted">where did this picture come from? <br></br>who or what created it?<br></br>is this genuine image or has it been tampered with?
             </p>
             <p className="description-text">
               <code>isitai</code> is a free web application that helps you answer these questions by parsing and extracting the binary metadata of digital images.  Simply attach a JPG, JPEG, PNG, HEIC, JP2, or TIFF file and <code>isitai</code> will instantly decode its embedded <code><Link href="https://c2pa.org/specifications/specifications/1.0/specs/_attachments/C2PA_Specification.pdf">C2PA</Link></code> and <code><Link href="https://www.cipa.jp/std/documents/download_e.html?cipa_dc-008-2024-e">EXIF</Link></code> metadata such as origin, modification date, gps location, camera info, and more.  Empower yourself to see if an image was manipulated, generated artificially, or was genuinely captured by a camera lens.
@@ -43,7 +43,18 @@ export default function Page() {
         </section>
       </main>
       <footer className="app-footer">
-        <p>&copy; {new Date().getFullYear()} Morgan Bergen. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()}{' '}
+          <a
+            href="https://www.linkedin.com/in/morganbergen"
+            style={{ textDecoration: 'underline' }}
+            target="_blank" // Opens the link in a new tab
+            rel="noopener noreferrer" // Security measure for target="_blank"
+          >
+            Morgan Bergen
+          </a>
+          . All rights reserved.
+        </p>
       </footer>
     </div>
   );
